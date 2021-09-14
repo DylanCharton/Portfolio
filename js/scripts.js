181284cc78cd6b57f83cd4b810f7cc54444c6145
DylanCharton <d.charton@codeur.online>
2021-09-14 16:53:09 +0200

    Début du JS

--- js/scripts.js
+++ js/scripts.js
@@ -53,61 +53,14 @@ window.addEventListener('DOMContentLoaded', event => {
 
 });
 
-// Typewriter effect
+// Scroll reveal
+ScrollReveal().reveal('.letter');
+ScrollReveal().reveal('#my-photo', {delay: 1500});
+ScrollReveal().reveal('#what-i-am', {delay: 600});
+ScrollReveal().reveal('#seeking', {delay: 900});
+ScrollReveal().reveal('#about-title', {delay: 400});
+ScrollReveal().reveal('.about-first', {delay: 500});
+ScrollReveal().reveal('.about-second', {delay: 500});
+ScrollReveal().reveal('.about-img');
+ScrollReveal().reveal('.about-img');
 
-let TxtType = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
-};
-
-TxtType.prototype.tick = function() {
-    let i = this.loopNum % this.toRotate.length;
-    let fullTxt = this.toRotate[i];
-
-    if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-
-    this.el.innerHTML = '<span class="wrap">J\'aime '+this.txt+'</span>';
-
-    let that = this;
-    let delta = 200 - Math.random() * 100;
-
-    if (this.isDeleting) { delta /= 2; }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-    }
-
-    setTimeout(function() {
-    that.tick();
-    }, delta);
-};
-
-window.onload = function() {
-    let elements = document.getElementsByClassName('typewrite');
-    for (let i=0; i<elements.length; i++) {
-        let toRotate = elements[i].getAttribute('data-type');
-        let period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-          new TxtType(elements[i], JSON.parse(toRotate), period);
-        }
-    }
-    // INJECT CSS
-    let css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-    document.body.appendChild(css);
-};
